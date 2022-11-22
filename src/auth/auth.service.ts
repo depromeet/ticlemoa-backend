3a4f4fbe20288df18e82bd0a00d3e048c66ae6cf
Seongtae <stae1102@gmail.com>
2022-11-22 14:28:38 +0900

    feat: kakao login 수행 api 서비스 작성
    기능만 구현했으며, 주석 및 타입 지정은 향후 진행할 예정입니다.
    유저 유효성 검사는 typeorm 연결 후 수행하겠습니다.
    
    Fixes: #3

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Profile } from 'passport-kakao';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async validateUser(profile: Profile['_json']) {
+    // const profile
+    // const exUser = await this.userRepository.findOne({
+    //   where: { email },
+    //   select: ['id', 'email', 'nickname'],
+    // });
+    // if (!exUuser) {
+    //   const newUser = await this.userRepository.create({
+    //     id: profile.id,
+    //     email: profile.has_email,
+    //     nickname: profile.nickname,
+    //     provider: 'kakao',
+    //   });
+    //   return newUser;
+    // }
+    // return exUser;
+    return true;
+  }
+
+  async logIn(user, res) {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.generateAccessToken(user.id),
+      this.generateRefreshToken(user.id),
+    ]);
+
+    res.cookie('refresh_token', refreshToken, {
+      path: '/auth',
+      httpOnly: true,
+    });
+
+    return { accessToken };
+  }
+
+  async generateAccessToken(user) {
+    return this.jwtService.signAsync(
+      { user },
+      {
+        expiresIn: '1m',
+        subject: 'ACCESS_TOKEN',
+      },
+    );
+  }
+
+  async generateRefreshToken(user) {
+    return this.jwtService.signAsync(
+      { user },
+      {
+        expiresIn: '30 days',
+        subject: 'REFRESH_TOKEN',
+      },
+    );
+  }
+}
