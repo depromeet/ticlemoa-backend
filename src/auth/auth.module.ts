66899a8d1042ffafe9732da762f758750456c15e
Seongtae <stae1102@gmail.com>
2022-11-22 14:24:05 +0900

    feat: auth 모듈에 JwtModule 임포트

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { KakaoStrategy } from './strategies/kakao.strategy';
+import { KakaoStrategy } from './utils/strategies/kakao.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, KakaoStrategy],
 })
