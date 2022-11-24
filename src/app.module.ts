fa0888943b9af00f172cfd849731e17895ea2841
Seongtae <stae1102@gmail.com>
2022-11-24 12:12:42 +0900

    feat(app): typeorm 등록
    Fixes: #3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { mySqlOptions } from './ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TypeOrmModule.forRoot(mySqlOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
