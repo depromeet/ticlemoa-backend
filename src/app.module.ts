6c51abd97bd656ffc549ce4d1e483eec87498015
Seongtae <stae1102@gmail.com>
2022-11-26 16:07:39 +0900

    chore: 사용하지 않는 app controller, service 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { mySqlOptions } from './ormconfig';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TypeOrmModule.forRoot(mySqlOptions)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
