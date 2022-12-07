d63e7732b9276b99fb0d0900637427536f624d96
Seongtae <stae1102@gmail.com>
2022-12-07 21:28:48 +0900

    chore: 사용하지 않는 dto, interface 파일 삭제
    - validate-user.dto.ts
    - accessToken.interface.ts
    
    Fixes: #17

--- src/auth/dto/validate-user.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PickType } from '@nestjs/mapped-types';
-import { User } from '../../entities/user.entity';
-
-export class ValidateUserDto extends PickType(User, ['snsId', 'email', 'nickname', 'provider'] as const) {}
