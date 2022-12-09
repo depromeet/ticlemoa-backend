d71a6db39bce9697db3a221212479e5aa1c265d6
Be-student <presentsong@naver.com>
2022-12-10 02:05:54 +0900

    feat: update 부분도 create와 동일한 형식을 사용하도록 변경한다

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ArticleService } from './article.service';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
-import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { ParseIdsPipe } from 'src/common/decorators/ids.pipe';
+import { ArticleService } from './article.service';
 
+@ApiTags('article')
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
+  //Todo: 가드 작업 완료 후 추가할 예정
+  // @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articleService.create(createArticleDto);
-  }
+  create(@Body() createArticleDto: CreateArticleDto) {}
 
   @Get()
-  findAll() {
-    return this.articleService.findAll();
-  }
+  findAll() {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.articleService.findOne(+id);
-  }
+  @Get(':userId')
+  findOne(@Param('userId') id: string) {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
-    return this.articleService.update(+id, updateArticleDto);
-  }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateArticleDto: CreateArticleDto) {}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.articleService.remove(+id);
-  }
+  // @UseGuards(JwtAuthGuard)
+  @ApiParam({
+    name: 'ids',
+    description: '제거할 id들을 받아옵니다. bulk api일 경우 1,2,3,4 가 가능함',
+    example: '1,2,3,4',
+  })
+  @Delete(':ids')
+  remove(@Param('ids', new ParseIdsPipe()) id: Array<number>) {}
 }
