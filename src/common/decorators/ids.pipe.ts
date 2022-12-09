02b6f2e23389fa8ac7f4252a430f8cf6c87d1064
Be-student <presentsong@naver.com>
2022-12-10 02:05:21 +0900

    feat: id를 파싱하는 파이프 추가

--- src/common/decorators/ids.pipe.ts
+++ src/common/decorators/ids.pipe.ts
@@ -2,7 +2,7 @@ import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
 import { BadRequestException } from '@nestjs/common/exceptions/bad-request.exception';
 
 @Injectable()
-export class RegexTest implements PipeTransform {
+export class ParseIdsPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata): Array<number> {
     if (!/^\d+(,\d+)*$/.test(value)) {
       throw new BadRequestException('ids are not valid');
