b5cc02a40d1b357f6ee00dc4d3b08a4d7cf4f827
Be-student <presentsong@naver.com>
2022-12-09 22:41:03 +0900

    feat: health check 태그 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get('/')
+  @ApiTags('health check')
   healthCheck(): string {
     return 'ok';
   }
