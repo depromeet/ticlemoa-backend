bdf1dccc222763839b478599f89f1d79a51daa58
Be-student <presentsong@naver.com>
2022-12-09 21:14:54 +0900

    feat: nestcli 로 article resource 를 생성한다

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { mySqlOptions } from './ormconfig';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TypeOrmModule.forRoot(mySqlOptions)],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TypeOrmModule.forRoot(mySqlOptions), ArticleModule],
   controllers: [AppController],
 })
 export class AppModule {}
