8cd2ec20ab1ba7e67743f776c9acda0317314882
Be-student <presentsong@naver.com>
2022-12-11 20:36:10 +0900

    fix: 현재 user 생성 과정이 없기에 임시로 생성하는 기능을 추가한다

--- src/article/domain/ArticleMapper.ts
+++ src/article/domain/ArticleMapper.ts
@@ -11,7 +11,9 @@ export class ArticleMapper {
     // this.article.=articleCreateDto.tagIds
     this.article.title = articleCreateDto.title;
     this.article.link = articleCreateDto.url;
-    this.article.userId = articleCreateDto.userId;
+    //Todo : userId 생성 과정을 밟고 난 이후에는 수정할 것
+    // this.article.userId = articleCreateDto.userId;
+    this.article.userId = 1;
     if (id) {
       this.article.id = id;
     }
