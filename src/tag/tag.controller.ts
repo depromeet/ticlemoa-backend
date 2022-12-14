2bc9a3ae07e080750b68166128b1ba2f6cd59dbb
Seongtae <stae1102@gmail.com>
2022-12-14 17:56:13 +0900

    docs(tag): swagger 응답 및 entity 속성 작성

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/utils/guards/jwt-auth.guard';
 import { UserRequest } from '../common/decorators/user-request.decorator';
 import { User } from '../entities/user.entity';
@@ -16,6 +16,7 @@ export class TagController {
   @UseGuards(JwtAuthGuard)
   @ApiOperation({ description: '태그를 생성합니다.' })
   @ApiCreatedResponse({ description: '태그 생성에 성공했습니다.', type: OneTagResponseDto })
+  @ApiBadRequestResponse({ description: '이미 존재하는 태그입니다.' })
   async createTag(@UserRequest() user: User, @Body() createTagRequest: CreateTagRequest): Promise<OneTagResponseDto> {
     return this.tagService.createTag(user.id, createTagRequest);
   }
