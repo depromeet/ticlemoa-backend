505a66f1f780cb5db6f0b7c58fc06c9c3f342d05
Seongtae <stae1102@gmail.com>
2022-12-14 17:56:49 +0900

    refactor(tag): 태그 생성 트랜잭션 제거

--- src/tag/repository/tag.repository.ts
+++ src/tag/repository/tag.repository.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
 import { Tag } from '../../entities/tag.entity';
 import { CreateTagDto } from '../dto/create-tag.dto';
+import { OneTagResponseDto } from '../dto/response-tag.dto';
 
 @Injectable()
 export class TagRepository extends Repository<Tag> {
@@ -13,27 +14,14 @@ export class TagRepository extends Repository<Tag> {
     super(Tag, dataSource.createEntityManager(), dataSource.createQueryRunner());
   }
 
-  async createNewTag(createTagDto: CreateTagDto): Promise<Tag> {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
-    try {
-      const existedTag: Tag = await this.findOne({ where: { tagName: createTagDto.tagName } });
-      if (existedTag) {
-        throw new BadRequestException({
-          message: '이미 존재하는 태그입니다.',
-          statusCode: HttpStatus.BAD_REQUEST,
-        });
-      }
-      const createdTag: Tag = await this.save({ ...createTagDto });
-      await queryRunner.commitTransaction();
-      return createdTag;
-    } catch (err) {
-      await queryRunner.rollbackTransaction();
-      throw err;
-    } finally {
-      await queryRunner.release();
+  async createOneTag(createTagDto: CreateTagDto): Promise<OneTagResponseDto> {
+    const existedTag: Tag = await this.findOne({ where: { tagName: createTagDto.tagName } });
+    if (existedTag) {
+      throw new BadRequestException({
+        message: '이미 존재하는 태그입니다.',
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
     }
+    return await this.save({ ...createTagDto });
   }
 }
