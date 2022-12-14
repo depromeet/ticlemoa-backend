f979be1b188fc4681d8aec0a4061e95997dc398c
Seongtae <stae1102@gmail.com>
2022-12-14 17:57:04 +0900

    chore: 태그 생성 api 반환타입 지정

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Tag } from '../entities/tag.entity';
 import { CreateTagRequest } from './dto/request-tag.dto';
+import { OneTagResponseDto } from './dto/response-tag.dto';
 import { TagRepository } from './repository/tag.repository';
 
 @Injectable()
 export class TagService {
   constructor(private readonly tagRepository: TagRepository) {}
 
-  async createTag(userId: number, createTagDto: CreateTagRequest): Promise<Tag> {
-    return await this.tagRepository.createNewTag({ userId, tagName: createTagDto.tagName });
+  async createTag(userId: number, createTagDto: CreateTagRequest): Promise<OneTagResponseDto> {
+    return await this.tagRepository.createOneTag({ userId, tagName: createTagDto.tagName });
   }
 }
