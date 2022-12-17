3b11ad9b211f362761843955923a45b9ce7adb49
Seongtae <stae1102@gmail.com>
2022-12-17 17:10:27 +0900

    docs(tag): dto에 api property 추가

--- src/tag/dto/request/create-tag.dto.ts
+++ src/tag/dto/request/create-tag.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTagDto {
+  @ApiProperty({ description: '유저 id', example: 1 })
   userId: number;
+
+  @ApiProperty({ description: '태그 이름', example: '디프만' })
   tagName: string;
 }
--- src/tag/dto/response/response-tag.dto.ts
+++ src/tag/dto/response/response-tag.dto.ts
@@ -1,10 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class OneTagResponseDto {
+  @ApiProperty({ description: '유저 고유 id', example: 1 })
   userId: number;
+
+  @ApiProperty({ description: '태그 id', example: 1 })
   id: number;
+
+  @ApiProperty({ description: '태그 이름', example: '디프만' })
   tagName: string;
+
+  @ApiProperty({ description: '태그 생성 시간' })
   createdAt: Date;
+
+  @ApiProperty({ description: '태그 수정 시간' })
   updatedAt: Date;
 
   constructor(property: { userId: number; id: number; tagName: string; createdAt: Date; updatedAt: Date }) {
