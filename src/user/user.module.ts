448e8f4edcb9df75a65e201bb2be5210a198857d
Be-student <presentsong@naver.com>
2023-01-12 01:13:13 +0900

    fix: import 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
 import { UserRepository } from './repository/user.repository';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserRepository],
+  controllers: [UserController],
+  providers: [UserRepository, UserService],
   exports: [UserRepository],
 })
 export class UserModule {}
