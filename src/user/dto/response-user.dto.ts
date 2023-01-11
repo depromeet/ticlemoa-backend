6bb77e04cc2b87de6bb1ce8a64b6f73f8a965ae6
Be-student <presentsong@naver.com>
2023-01-12 01:18:28 +0900

    fix: 안 쓰이는 부분 제거

--- src/user/dto/response-user.dto.ts
+++ src/user/dto/response-user.dto.ts
@@ -2,11 +2,5 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseUserDto {
   @ApiProperty({ nullable: true })
-  email?: string;
-
-  @ApiProperty({ nullable: true })
   nickname?: string;
-
-  @ApiProperty({ nullable: true })
-  avatarUrl?: string;
 }
